8ea647fbe695ebfb505599bbf48f222163b70992
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-14 22:25:28 +1200

    test default buffer

--- test/index.js
+++ test/index.js
@@ -29,3 +29,23 @@ tape('fixtures', function (t) {
   t.end()
 })
 
+tape('fixtures, with default buffer', function (t) {
+  fixtures.forEach(function (e, i) {
+    t.test('test:'+i, function (t) {
+
+      var bytes = Math.ceil(Math.ceil((e.ary.length*e.width) / 32)*4)
+      var buffer = packer(null, 0, e.ary, e.width)
+      var bits = e.ary.length * e.width
+
+      console.log('output', buffer.length, u.bufferToBits(buffer))
+      console.log(bits, e)
+
+      t.equal(u.bufferToBits(
+          buffer.slice(0, Math.ceil(bits/8))
+        ), e.bits)
+      t.end()
+    })
+  })
+  t.end()
+})
+
