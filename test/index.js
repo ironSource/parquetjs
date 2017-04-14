8b81beeab5e0280f5ae2e8dc7691ebba8cb9dc75
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-14 22:22:33 +1200

    test passing in buffer

--- test/index.js
+++ test/index.js
@@ -5,21 +5,27 @@ var tape = require('tape')
 var u = require('../util')
 var packer = require('../')
 
+function Z(n) {
+  var b = new Buffer(n); b.fill(0); return b
+}
+
 tape('fixtures', function (t) {
   fixtures.forEach(function (e, i) {
     t.test('test:'+i, function (t) {
-//      console.log(e)
+
       var bytes = Math.ceil(Math.ceil((e.ary.length*e.width) / 32)*4)
-      var buffer = packer(new Buffer(bytes), e.ary, e.width)
+      var buffer = packer(Z(bytes), e.ary, e.width)
+      var bits = e.ary.length * e.width
+
       console.log('output', buffer.length, u.bufferToBits(buffer))
-      t.equal(u.bufferToBits(buffer
-//        .slice(0, Math.ceil((e.ary.length*e.width)/8))
+      console.log(bits, e)
+
+      t.equal(u.bufferToBits(
+          buffer.slice(0, Math.ceil(bits/8))
         ), e.bits)
       t.end()
     })
   })
   t.end()
 })
-
-
 
