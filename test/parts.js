6703729e3f178c45a9548a76d8440fe95436b2af
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-21 12:23:27 +1200

    better tests

--- test/parts.js
+++ test/parts.js
@@ -66,11 +66,6 @@ tape('pullBits when value is more than one byte', function (t) {
 
 })
 
-//function appendBits(buffer, bitIndex, value, width) {
-//  for(var i = bitIndex % 8; i < 8; i += width)
-//    ;
-//}
-//
 //width  1: 1 1 1 1 1 1 1 1,
 //width  2: 2 2 2 2,
 //width  3: 3 3 2, 1 3 3 1, 2 3 3,
@@ -84,45 +79,6 @@ tape('pullBits when value is more than one byte', function (t) {
 //width 11: 8, 3 5, 6 2, 8, 1 7, 4 4, 7 1, 8, 2 6, 5 3, 8
 
 
-//this is no longer used
-//tape('number of bytes needed to pack width evenly', function (t) {
-//
-//  t.equal(steps(1), 1)
-//  t.equal(steps(2), 1)
-//  t.equal(steps(3), 3)
-//  t.equal(steps(4), 1)
-//  t.equal(steps(5), 5)
-//  t.equal(steps(6), 3)
-//  t.equal(steps(7), 7)
-//  t.equal(steps(8), 1)
-//  t.equal(steps(9), 9, '8 9 bit numbers in 8 bytes')
-//  t.equal(steps(10), 5)
-//  t.equal(steps(11), 11)
-//  t.equal(steps(12), 3)
-//  t.equal(steps(13), 13)
-//  t.equal(steps(14), 7)
-//  t.equal(steps(15), 15)
-//  t.equal(steps(16), 2, '16 bit number must be 2 byte')
-//  t.equal(steps(17), 17)
-//  t.equal(steps(18), 9)
-//  t.equal(steps(19), 19)
-//  t.equal(steps(20), 10)
-//  
-//  t.end()
-//})
-
-//
-//var items = [
-//  8,4,8,2,
-//  8,3,8,1,
-//
-//  8,5,11,3,
-//  2,8,8,2
-//]
-//var bytes = [
-//  1, 1, 3, 1,
-//  5, 6, 7, 1
-//]
 
 tape('8x 1, to various widths', function (t) {
   var items = []
@@ -156,16 +112,14 @@ tape('i%3 into bitwidth 3', function (t) {
   var inputs = []
   for(var i = 0; i < 20; i++)
     inputs.push(i%3)
-  console.log(inputs)
   var out = new Buffer(Math.ceil(10*3/8))
   var bitstring = u.bufferToBits(bp(out, 0, inputs, 3), 3)
-  console.log(out)
-//  t.equal(nice, '000,001,01 0,000,001,0 10,000,001 ,010,000,00 1,010,000,0 01,010,000 ,001,010,00 0,001,010,0 00,001,010 ,000,000,0'
   console.log(bitstring)
   t.end()
 })
 
 
+//data pulled from parquet-mr tests
 var BE = new Buffer('05028140a05028140a', 'hex')
 var LE = new Buffer('881021428408112244','hex')
 
@@ -174,14 +128,23 @@ tape('little-endian', function (t) {
   for(var i = 0; i < 24; i++)
     inputs.push(i%3)
 
-  console.log('outBE', bp(Z(9), 0, inputs, 3))
-  console.log('outLE', u.bufferToBits(bp.LE(Z(9), 0, inputs, 3)))
-  console.log('   LE', u.bufferToBits(LE))
-  console.log(LE)
-  console.log(BE)
-  console.log('BE', u.bufferToBits(BE))
+  t.deepEqual(bp.LE(Z(9), 0, inputs, 3), LE)
+  t.deepEqual(bp(Z(9), 0, inputs, 3), BE)
   t.end()
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
