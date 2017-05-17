ebe59a22ae85a64867afa092665ae615668358c3
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-17 20:21:25 +1200

    script to convert the output of parquet-tools/cat -j (json lines) back into parquet

--- tools/json-to-parquet.js
+++ tools/json-to-parquet.js
@@ -2,14 +2,16 @@ var File = require('pull-file')
 var Split = require('pull-split')
 var pull = require('pull-stream')
 var toPull = require('stream-to-pull-stream')
-var Encode = require('./encode')
+var Encode = require('../encode')
 var Group = require('pull-group')
 var MapLast = require('pull-map-last')
 var fs = require('fs')
 
-module.exports = function () {
-  return Split('\n', JSON.parse)
+function parseJsonLines () {
+  return Split('\n', JSON.parse, null, true)
 }
+
+module.exports = Parquet
 
 function Parquet (schema, rows) {
   var headers = Object.keys(schema)
@@ -28,13 +30,12 @@ function Parquet (schema, rows) {
 if(!module.parent) {
   pull(
     File(process.argv[2]),
-    module.exports(),
-    pull.take(50000),
-    //pull.take(1000),
+    parseJsonLines(),
     Parquet(JSON.parse(fs.readFileSync(process.argv[3]))),
     toPull.sink(process.stdout)
   )
 }
+
 
 
 
