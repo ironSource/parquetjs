6c5f46fef109a9887b225463f307b25a707e510d
Paul Asmuth <paul@asmuth.com>
2017-08-31 16:53:32 +0200

    write basic parquet thrift file metadata message

--- test/envelope.js
+++ test/envelope.js
@@ -1,6 +1,9 @@
 fs = require('fs');
 parquet = require('../lib/writer.js');
 
-var writer = new parquet.ParquetFileWriter('test.dat');
+var writer = new parquet.ParquetFileWriter('test.parquet');
 writer.end();
 
+// inspect file with
+// $ hadoop jar parquet-tools-1.9.0.jar meta test.parquet
+// $ hadoop jar parquet-tools-1.9.0.jar dump test.parquet
