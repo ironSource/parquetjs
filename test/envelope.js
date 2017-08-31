a11db1df299d371e0e6c474af1268d826b8275e0
Paul Asmuth <paul@asmuth.com>
2017-08-31 20:18:27 +0200

    store column type, move encoding code to lib/

--- test/envelope.js
+++ test/envelope.js
@@ -2,7 +2,7 @@ fs = require('fs');
 parquet = require('..');
 
 var schema = new parquet.ParquetSchema();
-schema.addColumn({name: "num"});
+schema.addColumn({name: "num", type: "INT64"});
 
 var writer = new parquet.ParquetFileWriter(schema, 'test.parquet');
 for (var i = 0; i < 10; ++i) {
