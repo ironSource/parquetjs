fdcb1856d6da0731350a809048364c0a8c5d3758
Paul Asmuth <paul@asmuth.com>
2017-09-03 17:35:58 +0200

    add remaining PLAIN encodings, work around JS thrift bug

--- test/envelope.js
+++ test/envelope.js
@@ -2,16 +2,16 @@
 var parquet = require('..');
 
 var schema = new parquet.ParquetSchema();
-//schema.addColumn({name: "name", type: "STRING"});
+schema.addColumn({name: "name", type: "BYTE_ARRAY"});
 schema.addColumn({name: "quantity", type: "INT64"});
-//schema.addColumn({name: "price", type: "DOUBLE"});
-//schema.addColumn({name: "date", type: "TIMESTAMP"});
-//schema.addColumn({name: "in_stock", type: "BOOLEAN"});
+schema.addColumn({name: "price", type: "DOUBLE"});
+schema.addColumn({name: "date", type: "INT64"});
+schema.addColumn({name: "in_stock", type: "BOOLEAN"});
 
 var writer = new parquet.ParquetFileWriter(schema, 'test.parquet');
 for (let i = 0; i < 32; ++i) {
   writer.appendRow({name: 'apples', quantity: i, price: 2.5, date: +new Date(), in_stock: true});
-  writer.appendRow({name: 'oranges', quantity: i * 2, price: 2.5, date: +new Date(), in_stock: true});
+  writer.appendRow({name: 'oranges', quantity: i * 2, price: 2.5, date: +new Date(), in_stock: false});
 }
 writer.end();
 
