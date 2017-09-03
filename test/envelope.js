fa95df65939455c00dd281bc160e7b9938c40db8
Paul Asmuth <paul@asmuth.com>
2017-09-03 18:14:31 +0200

    cleanups

--- test/envelope.js
+++ test/envelope.js
@@ -3,7 +3,7 @@ var parquet = require('..');
 
 var schema = new parquet.ParquetSchema({
   name:     { type: "BYTE_ARRAY" },
-  quantity: { type: "INT64" },
+  quantity: { type: "INT64", optional: false },
   price:    { type: "DOUBLE" },
   date:     { type: "INT64" },
   in_stock: { type: "BOOLEAN" }
@@ -12,7 +12,8 @@ var schema = new parquet.ParquetSchema({
 var writer = new parquet.ParquetFileWriter(schema, 'test.parquet');
 for (let i = 0; i < 32; ++i) {
   writer.appendRow({name: 'apples', quantity: i, price: 2.5, date: +new Date(), in_stock: true});
-  writer.appendRow({name: 'oranges', quantity: i * 2, price: 2.5, date: +new Date(), in_stock: false});
+  writer.appendRow({name: 'oranges', quantity: i * 2, price: 2.5, date: +new Date(), in_stock: true});
+  //writer.appendRow({name: 'kiwi', price: 4.2, date: +new Date(), in_stock: false});
 }
 writer.end();
 
