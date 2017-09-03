6ba1a67e03e72aff19cd0f020757819fed9c7568
Paul Asmuth <paul@asmuth.com>
2017-09-03 16:26:32 +0200

    write RLE with bit_width == 0, allowing us to read the column values for required columns from parquet java

--- test/envelope.js
+++ test/envelope.js
@@ -2,15 +2,17 @@
 var parquet = require('..');
 
 var schema = new parquet.ParquetSchema();
-schema.addColumn({name: "name", type: "STRING"});
+//schema.addColumn({name: "name", type: "STRING"});
 schema.addColumn({name: "quantity", type: "INT64"});
-schema.addColumn({name: "price", type: "DOUBLE"});
-schema.addColumn({name: "date", type: "TIMESTAMP"});
-schema.addColumn({name: "in_stock", type: "BOOLEAN"});
+//schema.addColumn({name: "price", type: "DOUBLE"});
+//schema.addColumn({name: "date", type: "TIMESTAMP"});
+//schema.addColumn({name: "in_stock", type: "BOOLEAN"});
 
 var writer = new parquet.ParquetFileWriter(schema, 'test.parquet');
-writer.appendRow({name: 'apples', quantity: 10, price: 2.5, date: +new Date(), in_stock: true});
-writer.appendRow({name: 'oranges', quantity: 10, price: 2.5, date: +new Date(), in_stock: true});
+for (let i = 0; i < 32; ++i) {
+  writer.appendRow({name: 'apples', quantity: i, price: 2.5, date: +new Date(), in_stock: true});
+  writer.appendRow({name: 'oranges', quantity: i * 2, price: 2.5, date: +new Date(), in_stock: true});
+}
 writer.end();
 
 // inspect the output file with
