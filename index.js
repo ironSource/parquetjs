d3d1f51556593574884d9136ce2b80931ccd8351
Paul Asmuth <paul@asmuth.com>
2017-09-06 16:47:56 +0200

    make shredRecord method public

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const writer = require('./lib/writer');
 const schema = require('./lib/schema');
+const shredder = require('./lib/shred');
 
 module.exports = {
   ParquetWriter: writer.ParquetWriter,
   ParquetEncoder: writer.ParquetEncoder,
   ParquetSchema: schema.ParquetSchema,
+  ParquetShredder: shredder
 };
