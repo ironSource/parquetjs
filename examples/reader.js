fb432bfe63ad1752f617e1d676e12c03146376e2
Paul Asmuth <paul@asmuth.com>
2017-09-06 21:06:59 +0200

    read parquet footer metadata

--- examples/reader.js
+++ examples/reader.js
@@ -2,6 +2,11 @@
 const parquet = require('..');
 
 parquet.ParquetReader.openFile('fruits.parquet', (err, reader) => {
+  if (err) {
+    console.log("ERR", err);
+    process.exit(1);
+  }
+
   console.log("opened", err, reader);
   reader.close();
 });
